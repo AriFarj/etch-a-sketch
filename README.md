c1dbfe948e7acb2c1dfdbd263bdc39b480ced0aa
Ari <24855858+AriFarj@users.noreply.github.com>
2025-01-01 21:47:33 +0200

    done

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 # etch-a-sketch
 Odin Project Etch a Sketch project
 
-1. Create a webpage of 16x16 grid of square div
-    - Create in JavaScript, not manually in HTML
-    - Place all divs in on 'container' div
-    - Adjust with Flexbox for appearance
-
-2. Set hover effect on square, leaving a pixelated trial through the grid(change the grid color)
-    - Add new class to square
-    -- chaning square background color
-
-3. Add button asking user for a NEW grid size, deleting the previous one and placing a new one instead.
--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-let gridSize = 12;
 const container = document.querySelector(".container");
-makeCells();
+const input = document.getElementById("inputSubmit");
+
 function makeCells() {
 
-    for (i = 0; i < gridSize; i++) {
-        const row = document.createElement(`div`);
-        row.classList.add(`row`);
-        container.appendChild(row)
-        for (j = 0; j < gridSize; j++) {
-            let column = document.createElement(`div`);
-            column.classList.add(`column`);
-            row.appendChild(column);
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    };
+    const gridSize = document.getElementById('inputBox').value;
+    if (gridSize < 2 || gridSize > 100)
+        alert(`${gridSize} is not a valid value, please enter a number between 2 and 100.`)
+    else {
+        for (i = 0; i < gridSize; i++) {
+            const row = document.createElement(`div`);
+            row.classList.add(`row`);
+            container.appendChild(row);
+            for (j = 0; j < gridSize; j++) {
+                let column = document.createElement(`div`);
+                column.classList.add(`column`);
+                row.appendChild(column);
+            }
         }
-    }
 
+        const gridCell = document.getElementsByClassName('column');
+        gridArray = Array.from(gridCell);
+
+        gridArray.forEach(function (gridSquare) {
+            const color = window.getComputedStyle(gridSquare).getPropertyValue('background-color')
+            gridSquare.addEventListener('mouseover', function (e) {
+
+                const red = randomInt(255);
+                const blue = randomInt(255);
+                const green = randomInt(255);
+                e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+            })
+        })
+    }
 }
 
-
-
-
-
+function randomInt(num) {
+    return Math.floor(Math.random() * num)
+}
